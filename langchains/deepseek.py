33316763284a2c403ab126f3231f88438917d652
Sam Ma <sam_newyork@163.com>
2026-06-16 11:33:26 +0800

    upgrade langchain and langgraph to 1.0.0

--- langchains/deepseek.py
+++ langchains/deepseek.py
@@ -1,7 +1,7 @@
 import os
 
 from langchain.agents import create_agent
-
+from langchain.messages import AIMessage, HumanMessage
 api_key_val = os.getenv("DEEPSEEK_API_KEY")
 
 def get_weather(city: str) -> str:
@@ -23,3 +23,17 @@ if __name__ == '__main__':
         }
     )
     print(result["messages"][-1].content_blocks)
+
+    # agent流式调用，输出"## Recent AI News Summary": xxxxx
+    for chunk in agent.stream(
+            {"messages": [{"role": "user", "content": "Search for AI news and summarize the findings"}]
+    }, stream_mode="values"):
+        # Each chunk contains the full state at that point
+        latest_message = chunk["messages"][-1]
+        if latest_message.content:
+            if isinstance(latest_message, HumanMessage):
+                print(f"User: {latest_message.content}")
+            elif isinstance(latest_message, AIMessage):
+                print(f"Agent: {latest_message.content}")
+        elif latest_message.tool_calls:
+            print(f"Calling tools: {[tc['name'] for tc in latest_message.tool_calls]}")
